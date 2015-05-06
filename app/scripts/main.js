d6f9e9e61a48b09150810b9d59d4a3ede9675cf5
pat <patrick.h.needham@gmail.com>
2015-05-06 15:41:10 -0400

    moved nav to a partial view to set the tone for a clean index.html

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,48 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+/*global devourr, $*/
+
+var trace = function(){
+  for(var i = 0; i < arguments.length; i++){
+    console.log(arguments[i]);
+  }
+};
+
+trace('Welcome to Devourr!');
+
+var App = App || {
+  url: 'http://localhost:3000/'
+},
+$container = $('#container'),
+
+Router = Backbone.Router.extend({
+  routes: {
+    'home': 'home',  //http://localhost:9000/#/home,
+    'users': 'users', // http://localhost:9000/users
+    'users/:id': 'user',  //http://localhost:9000/#/users/1
+    'projects': 'projects', //http://localhost:9000/#/projects
+    'projects/:id': 'project',  //http://localhost:9000/#/projects/1
+    'new-project': 'newProject',//http://localhost:9000/#/new-project
+    'update-project': 'updateProject',//http://localhost:9000/#/update-project
+    'tasks': 'tasks', //http://localhost:9000/#/tasks
+    'tasks/:id': 'task',  //http://localhost:9000/#/tasks/1
+  },
+
+  home: function(){
+    $container.empty();
+  },
+
+});
+
+var router = new Router();
+Backbone.history.start();
+
+$(document).ready(function(){
+  trace('Backbone is loaded!');
+
+  $container.empty().load('partials/nav.html',function(response,status,xhr){
+    var $nav = $('#navbar');
+
+  });
+
+
+});

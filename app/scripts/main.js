91143b70b308dae1e5934918abee4cd9c3152d1d
pat <patrick.h.needham@gmail.com>
2015-05-13 13:59:13 -0400

    edit form prefills with existing data, but broke the new post form

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -93,7 +93,8 @@ Router = Backbone.Router.extend({
         var point = locate.lastIndexOf('/');
         var post_id = parseInt(locate.substring(point+1, locate.length));
         trace('Editing post ID: ' + post_id + "!");
-        App.editpost(post_id);
+        var data = response.post;
+        App.editpost(post_id,data);
       });
 
     })
@@ -106,22 +107,22 @@ Router = Backbone.Router.extend({
 
   newpost: function(){
     $('#container').empty().load('partials/new-post-form.html', function(response,status,xhr){
-      var $form = $('#new-post-form');
-      $form.on('submit',function(event){
-        App.newPostForm(event,$form,router);
+      // $newform = $('#new-post-form');
+      $('#new-post-form').on('submit',function(event){
+        App.newPostForm(event,$newform,router);
       });
     });
   },
 
 });
 
-
 App.newPostForm = function(e,form,router){
   if(e.preventDefault) e.preventDefault();
-  var title = $(form).find("input[name='post-title']").val();
-  var description = $(form).find("input[name='post-description']").val();
-  var picture = $(form).find("input[name='post-picture']").val();
-  var location = $(form).find("input[name='post-location']").val();
+  $newform = $('#new-post-form');
+  var title = $newform.find("input[name='post-title']").val();
+  var description = $newform.find("input[name='post-description']").val();
+  var picture = $newform.find("input[name='post-picture']").val();
+  var location = $newform.find("input[name='post-location']").val();
   var user_id = 1;//localStorage.getItem('currentUser');
 
   App.newPostParams(title, description, picture, location, user_id, router);
@@ -144,7 +145,6 @@ App.newPostParams = function(title, description, picture, location, user_id, rou
       trace(jqXHR, textStatus, "complete post!!");
     },
     success: function(data, textStatus, jqXHR){
-      debugger
       router.navigate("posts/" + data.post.id,{trigger: true});
       trace(data,textStatus, jqXHR, "successful post!!");
     },
@@ -155,18 +155,24 @@ App.newPostParams = function(title, description, picture, location, user_id, rou
     trace(response, "posted post!!");
   }).fail(function(jqXHR, textStatus, thrownError){
     trace(jqXHR, textStatus, thrownError);
-    router.navigate("posts",{trigger: true});
+    // router.navigate("posts",{trigger: true});
   }).always(function(response){
     trace(response);
   });
 };
 
-App.editpost = function(post_id){
+App.editpost = function(post_id,data){
   trace('Post ID came thru on the router: ' + post_id + "!")
   $('#container').empty().load('partials/edit-post-form.html', function(response,status,xhr){
-    var $form = $('#edit-post-form');
-    $form.on('submit',function(event,post_id){
-      App.editPostForm(event,$form,router);
+    trace('data' + data);
+    trace('response' + response);
+    var $editform = $('#edit-post-form');
+    $editform.find("input[name='post-title']").val(data.title);
+    $editform.find("input[name='post-description']").val(data.description);
+    $editform.find("input[name='post-picture']").val(data.picture);
+    $editform.find("input[name='post-location']").val(data.location);
+    $editform.on('submit',function(event,post_id){
+      App.editPostForm(event,$editform,router);
     });
   });
 },
@@ -176,12 +182,12 @@ App.editPostForm = function(e,form,post_id){
   var locate = window.location.hash;
   var point = locate.lastIndexOf('/');
   var post_id = parseInt(locate.substring(point+1, locate.length));
-  debugger
   if(e.preventDefault) e.preventDefault();
-  var title = $(form).find("input[name='post-title']").val();
-  var description = $(form).find("input[name='post-description']").val();
-  var picture = $(form).find("input[name='post-picture']").val();
-  var location = $(form).find("input[name='post-location']").val();
+  var $editform = $('#edit-post-form');
+  var title = $editform.find("input[name='post-title']").val();
+  var description = $editform.find("input[name='post-description']").val();
+  var picture = $editform.find("input[name='post-picture']").val();
+  var location = $editform.find("input[name='post-location']").val();
   var user_id = 1;//localStorage.getItem('currentUser');
 
   App.editPostParams(title, description, picture, location, user_id, post_id, router);
@@ -211,10 +217,10 @@ App.editPostParams = function(title, description, picture, location, user_id, po
       trace("Backbone editpost: fail",jqXHR,error,exception);
     },
   }).done(function(response){
-    trace(response, "posted project!!");
+    trace(response, "posted!!");
   }).fail(function(jqXHR, textStatus, thrownError){
     trace(jqXHR, textStatus, thrownError);
-    router.navigate("posts",{trigger: true});
+    router.navigate("failed edit of post",{trigger: true});
   }).always(function(response){
     trace(response);
   });
@@ -227,7 +233,7 @@ $(document).ready(function(){
 
   var $nav = $('#navbar');
   $header.empty().load('partials/nav.html',function(response,status,xhr){
-
+    $nav.hide();
   });
 
 

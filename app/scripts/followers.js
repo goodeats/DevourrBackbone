798b18b48fa3dd33ce052a3342f44498c9b00c33
pat <patrick.h.needham@gmail.com>
2015-05-22 13:52:19 -0400

    changed follower to follow

--- app/scripts/followers.js
+++ app/scripts/followers.js
@@ -12,21 +12,20 @@ App = (function(module){
     trace('I am gonna follow user #' + id);
     var currentUser = 1;
     $.ajax({
-      url: 'http://localhost:3000' + '/users/' + id + '/followers',
+      url: 'http://localhost:3000' + '/users/' + currentUser + '/follows',
       type: 'POST',
       data: {
-        follower: {
-          following_user_id: currentUser,
-          user_id: id,
+        follow: {
+          follow_user_id: id,
+          user_id: currentUser,
         }
       },
     })
     .done(function() {
       console.log("successful follow");
     })
-    .fail(function(response, data) {
-      console.log("follow error" + response, data);
-      debugger
+    .fail(function(jqXHR, textStatus, errorThrown) {
+      console.log("follow error" + jqXHR, textStatus, errorThrown);
     });
 
   };

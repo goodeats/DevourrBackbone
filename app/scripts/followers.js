7ffa413c270f85bfd42c8be3aad440dac4efde4d
pat <patrick.h.needham@gmail.com>
2015-05-25 21:13:58 -0400

    follow/unfollow button toggles; script finds if current user is following; follow click assigns global follow_id variable or it will be assigned if current user is already following; unfollow deletes the follow_id

--- app/scripts/followers.js
+++ app/scripts/followers.js
@@ -7,6 +7,75 @@ var App = App || {
 
 App = (function(module){
 
+  var follow_id;
+
+  module.getFollowers = function(){
+    var user_id = App.findUserId();
+    $.ajax({
+      url: App.url + '/follows',
+      type: 'GET',
+    })
+    .done(function(response) {
+      module.findUserFollowers(response);
+    })
+    .fail(function() {
+      console.log("error getting all follows");
+    });
+  };
+
+  module.findUserFollowers = function(response){
+    var $followers = $('#statFollowers');
+    var myFollowers = [];
+    for (var i = 0; i < response.follows.length; i++) {
+      if (response.follows[i].follow_user_id === App.findUserId()){
+        myFollowers.push(response.follows[i])
+      }
+    };
+    $followers.text(myFollowers.length);
+    module.checkIfFollowing(myFollowers);
+  };
+
+  module.checkIfFollowing = function(response){
+    var currentUser = 1;
+    var status = false;
+    if (response.length > 0){
+      for (var i = 0; i < response.length; i++) {
+        if (response[i].user_id === currentUser){
+          follow_id = response[i].id;
+          status = true;
+        }
+      }
+    }
+    module.followStatus(status);
+  };
+
+  module.followStatus = function(status){
+    if (status){
+      $('#follow-user').hide();
+      $('#unfollow-user').show();
+    } else {
+      $('#unfollow-user').hide();
+      $('#follow-user').show();
+    }
+    module.toggleFollow();
+  };
+
+  module.toggleFollow = function(){
+
+    $('#unfollow-user').unbind('click');
+    $('#unfollow-user').on('click', function(){
+      App.deleteFollow();
+      $('#unfollow-user').toggle();
+      $('#follow-user').toggle();
+    });
+
+    $('#follow-user').on('click', function(){
+      App.newFollow(module.findUserId());
+      $('#unfollow-user').toggle();
+      $('#follow-user').toggle();
+    });
+  };
+
   module.newFollow = function(id){
     var currentUser = 1;
     $.ajax({
@@ -19,64 +88,17 @@ App = (function(module){
         }
       },
     })
-    .done(function() {
+    .done(function(response) {
       console.log("successful follow");
+      follow_id = response.follow.id;
     })
     .fail(function(jqXHR, textStatus, errorThrown) {
       console.log("follow error" + jqXHR, textStatus, errorThrown);
     });
   };
 
-  module.getFollowers = function(){
-    var user_id = App.findUserId();
-    $.ajax({
-      url: App.url + '/follows',
-      type: 'GET',
-    })
-    .done(function(response) {
-      module.showUserFollowerCount(response);
-      module.checkIfFollowing(response);
-    })
-    .fail(function() {
-      console.log("error");
-    });
-  };
-
-  module.showUserFollowerCount = function(response){
-    var $followers = $('#statFollowers');
-    var myFollowers = [];
-    for (var i = 0; i < response.follows.length; i++) {
-      if (response.follows[i].follow_user_id === App.findUserId()){
-        myFollowers.push(response.follows[i])
-      }
-    };
-    $followers.text(myFollowers.length);
-  };
-
-  module.checkIfFollowing = function(response){
+  module.deleteFollow = function(){
     var currentUser = 1;
-    var userFollowers = [];
-    for (var i = 0; i < response.follows.length; i++) {
-      if (response.follows[i].follow_user_id === App.findUserId()
-        && response.follows[i].user_id === currentUser){//lost internet
-        var $thisFollow = response.follows[i].id;
-
-        $('#follow-user').text('Unfollow');
-
-        $('#unfollow-user').on('click', function(){
-          App.deleteFollow($thisFollow);
-        });
-      } else {
-        $('#follow-user').on('click', function(){
-          App.newFollow(module.findUserId());
-        });
-      }
-    };
-  };
-
-  module.deleteFollow = function(follow_id){
-    var currentUser = 1;
-    trace('ima unfollow u');
     $.ajax({
       url: App.url + '/users/' + currentUser + '/follows/' + follow_id,
       type: 'DELETE',
@@ -85,7 +107,7 @@ App = (function(module){
       console.log("successful unfollow");
     })
     .fail(function() {
-      console.log("error deleting follower");
+      console.log("error deleting follow");
     });
 
   };
